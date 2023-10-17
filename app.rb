bc7e647f49517c1c721c95da0682643013bc77b5
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-10-17 23:15:27 +0000

    cranking

--- app.rb
+++ app.rb
@@ -3,7 +3,30 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>Dice Roll</h1>
   "
+  <form action="/process_roll">
+    <label for="zebra">"Number of dice to roll?"</label>
+    <input id="zebra" name="Number of dice to roll?">
+
+    <label for="giraffe">"How many sides per die?"</label>
+    <input id="giraffe" name="How many sides per die?">
+
+    <button>Roll</button>
+  </form>
+end
+
+get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:results)
 end
