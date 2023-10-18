449f0c53a4352fefa2c7efbf1d0a35454ae0784c
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-10-18 00:16:20 +0000

    done

--- app.rb
+++ app.rb
@@ -2,18 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  "
-  <form action="/process_roll">
-    <label for="zebra">"Number of dice to roll?"</label>
-    <input id="zebra" name="Number of dice to roll?">
-
-    <label for="giraffe">"How many sides per die?"</label>
-    <input id="giraffe" name="How many sides per die?">
-
-    <button>Roll</button>
-  </form>
+  erb(:homepage)
 end
 
 get("/process_roll") do
